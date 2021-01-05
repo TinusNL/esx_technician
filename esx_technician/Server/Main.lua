7e0df0c8b4bf79344f09e5e9915678db2e466559
TinusNL <66055492+TinusNL@users.noreply.github.com>
2021-01-05 20:58:50 +0100

    Update Main.lua

--- esx_technician/Server/Main.lua
+++ esx_technician/Server/Main.lua
@@ -3,20 +3,15 @@ ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterServerEvent('esx_technician:PayMoney')
-AddEventHandler('esx_technician:PayMoney', function(CurrentJob)
+AddEventHandler('esx_technician:PayMoney', function()
     xPlayer = ESX.GetPlayerFromId(source)
-    PlayerCoords = xPlayer.getCoords(true)
-    JobCoords = vector3(CurrentJob.X, CurrentJob.Y, CurrentJob.Z)
     PlayerJob = xPlayer.getJob()
 
-
-    if Vdist2(PlayerCoords, JobCoords) <= 1.5 then
-        if PlayerJob.name == "technician" then
-            if Config.MoneyType == true then
-                xPlayer.addMoney(Config.MoneyAmount)
-            else
-                xPlayer.addAccountMoney('bank', Config.MoneyAmount)
-            end
+    if PlayerJob.name == "technician" then
+        if Config.MoneyType == true then
+            xPlayer.addMoney(Config.MoneyAmount)
+        else
+            xPlayer.addAccountMoney('bank', Config.MoneyAmount)
         end
     end
 end)
